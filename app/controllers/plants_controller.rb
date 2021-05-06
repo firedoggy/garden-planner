55b28ef504fc2b72d9f0f283706209ad2a6dd693
Samuel <sammkenzie@gmail.com>
2021-05-06 14:15:45 -0500

    Created login view.

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,9 +1,11 @@
 class PlantsController < ApplicationController
 
     def index
+        @plants = Plant.all
     end
 
     def show
+        @plant = Plant.find_by(id: params(:id))
     end
 
     def new

c0e6be604db97fdcf53b847a13b562459a0ba63d
Samuel <sammkenzie@gmail.com>
2021-05-06 12:55:24 -0500

    Added signup and login routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :gardens
-  resources :plants
-  resources :users
+    get '/signup' => 'users#new'
+    post '/signup' => 'users#create'
+
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    post '/logout' => 'sessions#destroy'
+
+    resources :gardens
+    resources :plants
+    resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
